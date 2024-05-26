57b99813f7dc3a971fe2b314484ce6df26907608
Yong Seung Lee <leeyongs@umich.edu>
2024-05-26 16:27:42 -0400

    Pong

--- app/main.py
+++ app/main.py
@@ -9,7 +9,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+
+    print("Listening from port 6379")
+    while True:
+        conn_sock, client_addr = server_socket.accept()  # wait for client
+        print(f"Accepted the request from {client_addr}")
+        with conn_sock:
+            conn_sock.sendall("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
