1f105cc9b5616d531aa209f982d38c383bd0cf34
Yong Seung Lee <leeyongs@umich.edu>
2024-05-26 22:20:37 -0400

    Testing

--- app/request_handler.py
+++ app/request_handler.py
@@ -5,7 +5,7 @@ class RequestHandler:
     def handle(self, input: list | int | str) -> str:
         if isinstance(input, list) and isinstance(input[0], str):
             if input[0].upper() == "ECHO":
-                return RespParser.encode(input[1:])
+                return RespParser.encode(input[1])
             if input[0].upper() == "PING":
                 return RespParser.encode("PONG")
         raise ValueError("Unknown")
