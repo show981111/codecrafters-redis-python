498dfc0540076143f80c518b38f1b1b508c328cf
Yong Seung Lee <leeyongs@umich.edu>
2024-06-03 18:47:25 -0400

    Testing

--- app/rdb_parser.py
+++ app/rdb_parser.py
@@ -1,87 +1,136 @@
 import codecs
 from pathlib import Path
+from typing import Any, Tuple
 
 
 class RdbParser:
-    def __init__(self, file: Path):
-        self.db_file = file
+    """
+    Class to parse the redis db
+    """
+
+    def __init__(self, file_loc: Path):
+        self.index = 0
+        if not file_loc.is_file():
+            self.data = b""
+        else:
+            file = open(file_loc, "rb")
+            self.data = file.read()
+            file.close()
+        self.length = len(self.data)
         self.kv = {}
 
-    def _read_data(self) -> bytes:
-        if self.db_file.is_file():
-            f = open(self.db_file, "rb")
-            return f.read()
-        else:
-            print("File doesn't exist")
-            return b""
+    def inc(self) -> None:
+        self.index += 1
 
-    def _get_content(self, data: bytes) -> list:
-        if data == b"":
-            return None
-        res = data.hex(" ")
-        print(f"[Hex data] {res}")
-        idxofb = res.index("fb")
-        idxoff = res.index("ff")
-        d = res[idxofb:idxoff].split(" ")
-        print("[d]", d)
-        # lst = res[idxofb + 1 : idxoff].split("00")
-        # reslst = []
-        # for x in lst:
-        #     reslst.append(str(x).strip().split(" "))
-        # print("[lst]", lst)
-        # print("[reslst]", reslst)
-        # hash_table_size = int(reslst[0], 16)
-        # exp_hash_table_size = int(reslst[1], 16)
-        # exp_table: list = []
-        # while len(exp_table) < exp_hash_table_size:
-        #     exp_table.append()
-        # return reslst
+    def curr(self) -> bytes | None:
+        return self.data[self.index : self.index + 1]
 
-    def _extract_key_value_pairs(self, data: list):
-        if data == None:
-            return None
-        data.pop(0)
-        result = {}
-        for x in data:
-            if len(x) == 0:
-                continue
-            lengthKey = int(x[0])
-            l1 = x[1 : lengthKey + 1]
-            l2 = x[lengthKey + 1 :]
-            key = ""
-            value = ""
-            value_integer = False
-            for byt in l1:
-                key += byt
-            if "c0" in l2[0]:
-                value_integer = True
-                value = int(l2[1], 16)
+    def get(self, i) -> bytes:
+        return self.data[i : i + 1]
+
+    def substring(self, i, j) -> bytes:
+        return self.data[i:j]
+
+    def next(self, l: int) -> bytes:
+        return self.substring(self.index, self.index + l)
+
+    def read(self, l: int) -> bytes:
+        value = self.substring(self.index, self.index + l)
+        self.index += l
+        return value
+
+    def parse(self) -> dict:
+        if not self.data:
+            return self.kv
+        self.parse_redis()
+        version = self.parse_version()
+        print(f"RDB Version: {version}")
+        while self.has_next():
+            # print(self.data[self.index:])
+            print(self.data[self.index :])
+            if self.curr()[0] == 0xFA:
+                self.inc()
+                length, key = self.parse_string()
+                length, value = self.parse_string()
+                print(f"{key}: {value}")
+            elif self.curr()[0] == 0xFE:
+                self.inc()
+                database_number = self.parse_length()
+                print(f"Database Number: {database_number}")
+            elif self.curr()[0] == 0xFB:
+                self.inc()
+                db_ht_size = self.parse_length()
+                print(f"Database hash table size: {db_ht_size}")
+                exp_ht_size = self.parse_length()
+                print(f"Expiry hash table size: {exp_ht_size}")
+            elif self.curr()[0] == 0xFC:
+                self.inc()
+                expiry = int.from_bytes(self.read(8), byteorder="little")
+                value_type = self.read(1)[0]
+                length, key = self.parse_string()
+                length, value = self.parse_value(value_type)
+                print(f"Key:{key}, Value:{value}")
+                self.kv[key] = {"value": value, "expiry": expiry}
+            elif self.curr()[0] == 0xFF:
+                self.inc()
+                checksum = self.read(8)
                 break
             else:
-                l2.pop(0)
-                for byt in l2:
-                    value += byt
-            key = codecs.decode(key, "hex").decode("utf-8")
-            if not value_integer:
-                value = codecs.decode(value, "hex").decode("utf-8")
-            result[f"{key}"] = f"{value}"
-        return result
+                value_type = self.read(1)[0]
+                length, key = self.parse_string()
+                length, value = self.parse_value(value_type)
+                print(f"Key:{key}, Value:{value}")
+                self.kv[key] = {"value": value, "expiry": None}
+        return self.kv
 
-    def get_keys(self) -> list:
-        data = self._read_data()
-        print("Data read:", data)
-        if data == b"":
-            return []
-        print("the data is read")
-        trimmedData = self._get_content(data)
-        print("Trimmed data", trimmedData)
-        self.kv = self._extract_key_value_pairs(trimmedData)
-        print("key recieved", self.kv)
-        result = []
-        for k in self.kv:
-            result.append(k)
-        return result
+    def parse_redis(self):
+        if self.next(5) == b"REDIS":
+            self.index += 5
 
+    def parse_version(self):
+        value = int(self.read(4))
+        return value
 
-# p = RdbParser(Path("./file.bin"))
-# p.get_keys()
+    def parse_string(self) -> Tuple[int, Any]:
+        length = self.parse_length()
+        if length != -1:
+            string = self.read(length)
+            return 0, string.decode()
+        else:
+            self.index -= 1
+            b = ord(self.curr()) & 0b00111111
+            self.inc()
+            if b == 0b00:
+                val = int.from_bytes(self.read(1), byteorder="little")
+            elif b == 0b01:
+                val = int.from_bytes(self.read(2), byteorder="little")
+            elif b == 0b10:
+                val = int.from_bytes(self.read(4), byteorder="little")
+            else:
+                raise NotImplementedError
+            return 0, val
+
+    def parse_value(self, value_type):
+        if value_type == 0x00:
+            return self.parse_string()
+        else:
+            raise NotImplementedError
+
+    def parse_length(self) -> int:
+        b = ord(self.curr())
+        self.inc()
+        val = b >> 6
+        length = 0b00
+        if val == 0b00:
+            length = (b & 0b00111111).to_bytes(1, byteorder="little")
+        elif val == 0b01:
+            length = (b & 0b00111111).to_bytes(1, byteorder="little") + self.curr()
+            self.inc()
+        elif val == 0b10:
+            length = self.read(4)
+        else:
+            return -1
+        return int.from_bytes(length, byteorder="little")
+
+    def has_next(self) -> bool:
+        return self.index < self.length
